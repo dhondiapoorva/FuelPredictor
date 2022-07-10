9747928b02891f139b06a2de2396b15006179014
dhondiapoorva <adhondi@cougarnet.uh.edu>
2022-07-10 16:29:56 -0500

    assignemnt 3 business layer

---
+++ FuelQuoteApp_p1.EntModels/Models/Client.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+namespace FuelQuoteApp_p1.EntModels.Models
+{
+    public class Client
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string Address1 { get; set; }
+        public string Address2 { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+        public string Email { get; set; }
+        public User User_ID { get; set; }
+    }
+}
---
+++ FuelQuoteApp_p1.EntModels/Models/Quote.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace FuelQuoteApp_p1.EntModels.Models
+{
+    public class Quote
+    {
+        public int Id { get; set; }
+        public int GallonsRequested { get; set; }
+        public string DeliveryAddress { get; set; }
+        public DateTime DateRequested { get; set; }
+        public float PricePerGallon { get; set; }
+        public float TotalAmount { get; set; }
+        public User User { get; set; }
+    }
+}
